ad6a6b8666347b5c216966c88ca844a089d18bf2
vinace <vinsent.storage@gmail.com>
2019-05-06 18:07:58 +0530

    checking the from_numpy to load word_embedding

--- train.py
+++ train.py
@@ -87,10 +87,10 @@ idx2word = dict([(y, x) for x, y in word2idx.items()])
 # word_embedding_matrix = torch.from_numpy(np.array(word_embedding_matrix)).type(torch.float) # changed float32 to float
 # word_embedding_matrix = torch.from_numpy(np.array(word_embedding_matrix, dtype='f'))
 f'work embedding matrix type'
-print(type(word_embedding_matrix))
-print(dir(word_embedding_matrix))
-myset = set(word_embedding_matrix)
-print(myset)
+# print(type(word_embedding_matrix))
+# print(dir(word_embedding_matrix))
+# myset = set(word_embedding_matrix)
+# print(myset)
 
 word_embedding_matrix = torch.from_numpy(np.array(word_embedding_matrix)) # .float().to(device) # changed float32 to float
 char_embedding_matrix = torch.from_numpy(np.array(char_embedding_matrix)).type(torch.float)  # changed float32 to float
